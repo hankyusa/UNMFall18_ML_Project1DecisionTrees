97e222fd58e8fcc3709162eb80a4a4e57090ca88
Luke Hanks <hankyusa@gmail.com>
2018-08-31 08:48:18 -0600

    Add a function for testing the decision tree against the training data

--- ConstructionZone.py
+++ ConstructionZone.py
@@ -11,48 +11,14 @@ import math
 allFeatIDs = range(60)
 featVals = ['A','G','T','C','D','N','S','R']
 
-trainingDF = pd.read_csv("smallTraining.csv", header=None, index_col=0, names=['id','features','classification'])
-
-def getInstances(df, featID, featVal, classification=None):
-    if featID == None or featVal == None:
-        return df[df.classification == classification]
-    elif classification == None:
-        return df[(df.features.str[featID] == featVal)]
-    else:
-        return df[(df.features.str[featID] == featVal) & (df.classification == classification)]
-    
-
-df1 = getInstances(df=trainingDF, featID=2, featVal='A', classification='N')
-
-def entropy(df):
-    counts = list(df['classification'].value_counts())
-    if len(counts) <= 1:
-        return 0
-    props = [i/sum(counts) for i in counts]
-    result = 0
-    for p in props:
-        result = result - p * math.log(p,2)
-    return result
-
-ent = entropy(trainingDF)
-
-def infoGain(df,featID):
-    result = entropy(df)
-    for featVal in featVals:
-        S_v = getInstances(df,featID,featVal)
-        result = result - (len(S_v)/len(df)) * entropy(S_v)
-    return result
-
-#TODO write the tree class
-
 class DecisionNode:
     def __init__(self, inputDF, featIDs):
         self.df = inputDF
         self.featIDs = featIDs
         self.children = dict()
-#        gains = {featID : infoGain(trainingDF, featID) for featID in featIDs}
-#        featIDOfBestGain = max(gains, key=gains.get)
-        self.featIDOfBestGain = featIDs[0]
+        gains = {featID : infoGain(trainingDF, featID) for featID in featIDs}
+        self.featIDOfBestGain = max(gains, key=gains.get)
+#        self.featIDOfBestGain = featIDs[0]
         childFeatIDs = featIDs.copy()
         childFeatIDs.remove(self.featIDOfBestGain)
         if len(childFeatIDs) > 0:
@@ -70,5 +36,39 @@ class DecisionNode:
             return self.children[dna[self.featIDOfBestGain]].classify(dna)
         return self.classification
 
+def getInstances(df, featID, featVal, classification=None):
+    if featID == None or featVal == None:
+        return df[df.classification == classification]
+    elif classification == None:
+        return df[(df.features.str[featID] == featVal)]
+    else:
+        return df[(df.features.str[featID] == featVal) & (df.classification == classification)]
+
+def entropy(df):
+    counts = list(df['classification'].value_counts())
+    if len(counts) <= 1:
+        return 0
+    total = sum(counts)
+    props = [i/total for i in counts]
+    result = 0
+    for p in props:
+        result = result - p * math.log(p,2)
+    return result
+
+def infoGain(df,featID):
+    result = entropy(df)
+    for featVal in featVals:
+        S_v = getInstances(df,featID,featVal)
+        result = result - (len(S_v)/len(df)) * entropy(S_v)
+    return result
+
+trainingDF = pd.read_csv("extraSmallTraining.csv", header=None, index_col=0, names=['id','features','classification'])
+
 decisionTreeRootNode = DecisionNode(trainingDF, list(allFeatIDs))
+
+def testDecisionTree(instance):
+    return decisionTreeRootNode.classify(instance['features']) == instance['classification']
+
 print(decisionTreeRootNode.classify('AAAAAAAAAATAGCTGGGCATGGTGGCAGGCGCCTGTAGTTTCAGCTGCTTGGTGTCTGA'))
+
+temp = trainingDF.apply(testDecisionTree, axis=1)
